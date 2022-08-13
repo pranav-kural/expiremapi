7c1eaa5f421d599c3d201a47449dfba447c8ba2a
pranav-kural <pranavkural042@gmail.com>
2022-08-12 19:49:16 -0600

    define app S types

--- model/schemas/app_data_schema.js
+++ model/schemas/app_data_schema.js
@@ -1,6 +1,31 @@
 import S from "fluent-json-schema";
 
+// allow nullable types
+// https://ajv.js.org/json-schema.html#nullable
 
+export const getSType = (type) => {
+  // available types: https://ajv.js.org/json-schema.html#json-data-type
+  const defaultTypes = {
+    array: S.array,
+    bool: S.boolean,
+    enum: S.enum,
+    integer: S.integer,
+    object: S.object,
+    null: S.null,
+    number: S.number,
+    string: S.string,
+  };
+  const customTypes = {
+    uuid: S.string().format("uuid"),
+    email: S.string().format("email"),
+    timestamp: S.string().format("date-time"),
+  };
+  const STypes = {
+    ...defaultTypes,
+    ...customTypes,
+  };
+  return STypes[type];
+};
 
 const itemSchema = S.object()
   .prop("id", S.string().format(S.FORMATS.UUID).required())
