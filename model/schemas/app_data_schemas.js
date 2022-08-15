cffdf698efa1dde37f8b47153d5b2858e34a08ea
pranav-kural <pranavkural042@gmail.com>
2022-08-15 16:27:13 -0600

    added separate schema for adding an item

--- model/schemas/app_data_schemas.js
+++ model/schemas/app_data_schemas.js
@@ -1,10 +1,18 @@
 import { itemObjectProperties } from "./properties/item/item_object_properties.js";
-import { getSchemaProperties } from "./utils/schema_generator.js";
+import { getSchemaProperties } from "../utils/schema_generator.js";
 
 class AppDataSchemas {
   getItemObjectSchema = () => ({
     type: "object",
     ...getSchemaProperties(itemObjectProperties),
+  });
+  getAddItemObjectSchema = () => ({
+    type: "object",
+    ...getSchemaProperties(itemObjectProperties),
+    required: ["name"], // requires only name, not id
+    not: {
+      required: ["id"],
+    },
   });
 }
 
