59d3ede4a409a2d69c7663fc3bc4cf50be6382e5
pranav-kural <pranavkural042@gmail.com>
2022-08-15 16:01:44 -0600

    added uuid for dev & completed addItem

--- controllers/items/items_controller.js
+++ controllers/items/items_controller.js
@@ -1,5 +1,6 @@
-import AppDataSource from "../../model/app_data_source.js";
-import { validateItemObject } from "../../model/schemas/utils/validate_properties.js";
+import AppDataSource from "../../model/data/app_data_source.js";
+import { validateAddItemObject } from "../../model/utils/validate_properties.js";
+import { v4 as uuidv4 } from "uuid";
 
 class ItemsController {
   _appDataSource;
@@ -16,14 +17,31 @@ class ItemsController {
     return this._appDataSource.items.find((el) => el.id === id);
   }
 
+  /**
+   * Adds an item to user's items collection
+   * Validation: checks item object for schema validation
+   * @param {item object} item item object to be added
+   * @returns object containing item object if added successfully, else error
+   */
   addItem(item) {
     try {
-      const validItemObject = validateItemObject(item);
-      if (validItemObject) this._appDataSource.items.push(item);
-      else throw Error("item object validation failed");
-      return { itemAdded: item };
-    } catch (error) {
-      return { error };
+      const { validationSuccess, validationErrors } =
+        validateAddItemObject(item);
+      if (validationSuccess && !validationErrors) {
+        // generate item id
+        const itemId = uuidv4();
+        // add id property to item object
+        item = { id: itemId, ...item };
+        // add new item
+        this._appDataSource.items.push(item);
+        // return added item (including item id)
+        return { itemAdded: item };
+      } else {
+        const error = validationErrors.pop();
+        return { error: `${error.schemaPath} ${error.message}` };
+      }
+    } catch (err) {
+      throw err;
     }
   }
 }
