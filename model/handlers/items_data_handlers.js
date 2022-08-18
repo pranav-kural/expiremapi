55a664471e1e448b1de4f48d335e5b9de0b5da95
pranav-kural <pranavkural042@gmail.com>
2022-08-17 18:16:21 -0600

    added items data handler to handler items data requests

--- model/handlers/items_data_handlers.js
+++ model/handlers/items_data_handlers.js
@@ -1,41 +1,62 @@
 // import app's data source
 import AppDataSource from "../data/app_data_source.js";
+// import method for validating items data
+import itemsValidationHandlers from "./items_validation_handlers.js";
+import itemsActionHandler, { ITEMS_ACTIONS } from "./items_action_handlers.js";
+import { v4 as uuidv4 } from "uuid";
 // get items data source from the app's data source
 const itemsDataSource = AppDataSource.getItemsDataSource();
 
-const getAllItems = () => itemsDataSource.items;
+const getAllItems = () =>
+  itemsActionHandler.dispatch(ITEMS_ACTIONS.GET_ALL_ITEMS);
 
-const updateItems = (items) => (itemsDataSource.items = items);
+const getItemById = (id) =>
+  itemsActionHandler.dispatch(ITEMS_ACTIONS.GET_ITEM_BY_ID, id);
 
-const getItemById = (id) => itemsDataSource.items.find((el) => el.id === id);
-
-const addItem = (item) => itemsDataSource.items.push(item);
+const addItem = (item) => {
+  try {
+    const { validationSuccess, validationErrors } =
+      itemsValidationHandlers.validateAddItemObject(item);
+    if (validationSuccess && !validationErrors) {
+      // generate item id
+      const itemId = uuidv4();
+      // add id property to item object
+      item = { id: itemId, ...item };
+      // add new item
+      itemsActionHandler.dispatch(ITEMS_ACTIONS.ADD_ITEM, item);
+      // return added item (including item id)
+      return { itemAdded: item };
+    } else {
+      return {
+        error: `validation failed for addItem: ${validationErrors.message}`,
+      };
+    }
+  } catch (err) {
+    throw err;
+  }
+};
 
 /**
  * Updates an item. Updates only the properties provided.
  * To update an item completely, i.e., remove existing properties,
  * delete the item and create a new one.
  * @param {object} updatedItem item object with updated values
- * @returns item object with all its properties if item was found and updated, else false
+ * @returns item object with all its properties if item was found and updated, else object containing 'error'
  */
 const updateItem = (updatedItem) => {
-  let itemUpdated = false;
-  // update item if present and update itemUpdated
-  const filteredItems = itemsDataSource.items.map((currentItem) => {
-    if (currentItem.id === updatedItem.id) {
-      itemUpdated = {
-        ...currentItem,
-        ...updatedItem,
-      };
-      return itemUpdated;
-    }
-    return currentItem;
-  });
-  // update items if itemUpdated not null (item was found)
-  if (itemUpdated) updateItems(filteredItems);
-  // item object with all its properties if item was found and updated,
-  // else returns false
-  return itemUpdated;
+  try {
+    // validate item object
+    const { validationSuccess, validationErrors } =
+      itemsValidationHandlers.validateItemObject(item);
+    // call update item action; returns object containing updated item or error
+    return validationSuccess
+      ? itemsActionHandler.dispatch(ITEMS_ACTIONS.UPDATE_ITEM, updatedItem)
+      : {
+          error: `validation failed for the provided item object: ${validationErrors.message}`,
+        };
+  } catch (err) {
+    throw err;
+  }
 };
 
 /**
@@ -44,19 +65,18 @@ const updateItem = (updatedItem) => {
  * @returns item object if item found and deleted, else boolean false
  */
 const deleteItem = (itemId) => {
-  let itemDeleted = false;
-  // delete item if present and update itemDeleted
-  const filteredItems = itemsDataSource.items.filter((currentItem) => {
-    if (currentItem.id === itemId) {
-      itemDeleted = currentItem;
-      return false;
-    }
-    return true;
-  });
-  // update items if itemDeleted not null (item was found)
-  if (itemDeleted) updateItems(filteredItems);
-  // itemDeleted contains the item deleted if found, else boolean false
-  return itemDeleted;
+  try {
+    // validate item id
+    const { validationSuccess, validationErrors } =
+      itemsValidationHandlers.validateItemId(itemId);
+    return validationSuccess
+      ? itemsActionHandler.dispatch(ITEMS_ACTIONS.DELETE_ITEM, itemId)
+      : {
+          error: `validation failed for the provided item id: ${validationErrors.message}`,
+        };
+  } catch (err) {
+    throw err;
+  }
 };
 
 export default {
